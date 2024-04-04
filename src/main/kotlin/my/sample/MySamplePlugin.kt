1e17fe9e6bc3377dcc7c5c3e6cb896466bb4e812
Tom Koptel <tom.koptel@gmail.com>
2024-04-04 14:05:33 +0200

    Add dummy inlined code

--- src/main/kotlin/my/sample/MySamplePlugin.kt
+++ src/main/kotlin/my/sample/MySamplePlugin.kt
@@ -2,7 +2,11 @@ package my.sample
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.the
+import org.gradle.kotlin.dsl.withType
+import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -13,6 +17,13 @@ class MySamplePlugin : Plugin<Project> {
             description = "A sample task"
             input.set(SimpleDateFormat("yyyy-MM-dd").format(Date()))
             output.set(layout.buildDirectory.file("mySampleTaskOutput.txt"))
+        }
+
+        // dummy inlined code
+        tasks.withType<Test> {
+             the<JacocoTaskExtension>().apply {
+                 logger.lifecycle("JacocoTaskExtension: $this")
+             }
         }
         Unit
     }
